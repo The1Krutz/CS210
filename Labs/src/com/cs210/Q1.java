c7815a34741df246c87697efa6ddd12493b5e7ac
The1Krutz <TKent@outlook.com>
2016-09-20 22:13:22 -0700

    Chapter 1 Lab Handout

---
+++ Labs/src/com/cs210/Q1.java
@@ -0,0 +1,8 @@
+package com.cs210;
+
+public class Q1 {
+
+    public static void main(String[] args) {
+        System.out.println("What is the difference between\na \' and a \"? Or between a \" and a \\\"?\n\nOne is what we see when we're typing our program.\nThe other is what appears on the \"console.\"");
+    }
+}
