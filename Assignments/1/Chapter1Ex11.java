61a66b37170197e61387a89f4de5a9ba4ab130f8
Thomas <the1krutz@gmail.com>
2016-10-03 17:45:37 -0700

    added ability to use command line args to determine number of rockets.

--- Assignments/1/Chapter1Ex11.java
+++ Assignments/1/Chapter1Ex11.java
@@ -2,9 +2,19 @@
 // Thomas / Kent
 
 public class Chapter1Ex11 {
-    public static int numRockets = 2;
+    // public static int numRockets = 2;
 
     public static void main(String[] args) {
+        int numRockets = 2; // default value in case no args are given
+
+        try {
+            // System.out.println(args[0]);
+            numRockets = Integer.parseInt(args[0]);
+        } catch (Exception ex) {
+            // System.err.println(ex.toString());
+            numRockets = 2;
+        }
+
         drawCone(numRockets);
         drawDivider(numRockets);
 
@@ -24,16 +34,16 @@ public class Chapter1Ex11 {
      * Draws a rocket cone
      * @param num number of times to draw this figure
      */
-    public static void drawCone(int num){
-        for(int i=0; i<num; i++) {
+    public static void drawCone(int num) {
+        for (int i = 0; i < num; i++) {
             System.out.print("   /\\    ");
         }
         System.out.println();
-        for(int i=0; i<num; i++) {
+        for (int i = 0; i < num; i++) {
             System.out.print("  /  \\   ");
         }
         System.out.println();
-        for(int i=0; i<num; i++) {
+        for (int i = 0; i < num; i++) {
             System.out.print(" /    \\  ");
         }
         System.out.println();
@@ -43,12 +53,12 @@ public class Chapter1Ex11 {
      * Draws a rocket body
      * @param num number of times to draw this figure
      */
-    public static void drawBody(int num){
-        for(int i=0; i<num; i++) {
+    public static void drawBody(int num) {
+        for (int i = 0; i < num; i++) {
             System.out.print("|      | ");
         }
         System.out.println();
-        for(int i=0; i<num; i++) {
+        for (int i = 0; i < num; i++) {
             System.out.print("|      | ");
         }
         System.out.println();
@@ -58,12 +68,12 @@ public class Chapter1Ex11 {
      * Draws a rocket body with the words 'United States' inside it
      * @param num number of times to draw this figure
      */
-    public static void drawBodyWithWords(int num){
-        for(int i=0; i<num; i++) {
+    public static void drawBodyWithWords(int num) {
+        for (int i = 0; i < num; i++) {
             System.out.print("|United| ");
         }
         System.out.println();
-        for(int i=0; i<num; i++) {
+        for (int i = 0; i < num; i++) {
             System.out.print("|States| ");
         }
         System.out.println();
@@ -73,8 +83,8 @@ public class Chapter1Ex11 {
      * Draws a horizontal divider
      * @param num number of times to draw this figure
      */
-    public static void drawDivider(int num){
-        for(int i=0; i<num; i++) {
+    public static void drawDivider(int num) {
+        for (int i = 0; i < num; i++) {
             System.out.print("+------+ ");
         }
         System.out.println();
