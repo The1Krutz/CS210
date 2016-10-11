51b07910efaad9b0e03b26725c62819f0a87b017
Thomas <the1krutz@gmail.com>
2016-10-10 17:23:38 -0700

    finished ascii art

--- Assignments/2/AsciiArt.java
+++ Assignments/2/AsciiArt.java
@@ -41,153 +41,153 @@ public class AsciiArt {
      * Draws the first line of art
      */
     public static void drawLine1() {
-        System.out.println("                  ,'  `.");
+        System.out.println("                 ,'  `.");
     }
 
     /**
      * Draws the second line of art
      */
     public static void drawLine2() {
-        System.out.println("                 (   ,. `.");
+        System.out.println("                (   ,. `.");
     }
 
     /**
      * Draws the third line of art
      */
     public static void drawLine3() {
-        System.out.println("               ,'.`. `.`. `.                       ssssssssss        uuuuuu    uuuuuu       nnnn  nnnnnnnn");
+        System.out.println("              ,'.`. `.`. `.                       ssssssssss        uuuuuu    uuuuuu       nnnn  nnnnnnnn");
     }
 
     /**
      * Draws the fourth line of art
      */
     public static void drawLine4() {
-        System.out.println("             ,.`. `.`. `.`. `.                   ss::::::::::s       u::::u    u::::u       n:::nn::::::::nn");
+        System.out.println("            ,.`. `.`. `.`. `.                   ss::::::::::s       u::::u    u::::u       n:::nn::::::::nn");
     }
 
     /**
      * Draws the fifth line of art
      */
     public static void drawLine5() {
-        System.out.println("            '. `.`. `.`. `.`. `,               ss:::::::::::::s      u::::u    u::::u       n::::::::::::::nn");
+        System.out.println("           '. `.`. `.`. `.`. `,               ss:::::::::::::s      u::::u    u::::u       n::::::::::::::nn");
     }
 
     /**
      * Draws the sixth line of art
      */
     public static void drawLine6() {
-        System.out.println("          ,`. `. `.`. `.`. `.`' ,`.            s::::::ssss:::::s     u::::u    u::::u       nn:::::::::::::::n");
+        System.out.println("         ,`. `. `.`. `.`. `.`' ,`.            s::::::ssss:::::s     u::::u    u::::u       nn:::::::::::::::n");
     }
 
     /**
      * Draws the seventh line of art
      */
     public static void drawLine7() {
-        System.out.println("        ,' ,',. `. `.`. `.`,' ,' ,',.           s:::::s  ssssss      u::::u    u::::u         n:::::nnnn:::::n");
+        System.out.println("       ,' ,',. `. `.`. `.`,' ,' ,',.           s:::::s  ssssss      u::::u    u::::u         n:::::nnnn:::::n");
     }
 
     /**
      * Draws the eighth line of art
      */
     public static void drawLine8() {
-        System.out.println("      ,' ,',' ,`__`. `'   ) ,' ,',' ,`__          s::::::s           u::::u    u::::u         n::::n    n::::n");
+        System.out.println("     ,' ,',' ,`__`. `'   ) ,' ,',' ,`__          s::::::s           u::::u    u::::u         n::::n    n::::n");
     }
 
     /**
      * Draws the ninth line of art
      */
     public static void drawLine9() {
-        System.out.println("    ,' ,',' ,','  `.`.__.',' ,',' ,','  `.           s::::::s        u::::u    u::::u         n::::n    n::::n");
+        System.out.println("   ,' ,',' ,','  `.`.__.',' ,',' ,','  `.           s::::::s        u::::u    u::::u         n::::n    n::::n");
     }
 
     /**
      * Draws the tenth line of art
      */
     public static void drawLine10() {
-        System.out.println("  ,' ,',' ,',' ,.   )   ,' ,',' ,',' ,.   )    ssssss   s:::::s      u:::::uuuu:::::u         n::::n    n::::n");
+        System.out.println(" ,' ,',' ,',' ,.   )   ,' ,',' ,',' ,.   )    ssssss   s:::::s      u:::::uuuu:::::u         n::::n    n::::n");
     }
 
     /**
      * Draws the eleventh line of art
      */
     public static void drawLine11() {
-        System.out.println(" (   `' ,',' ,',' ,'__ (   `' ,',' ,',' ,'     s:::::ssss::::::s     u:::::::::::::::uu       n::::n    n::::n");
+        System.out.println("(   `' ,',' ,',' ,'__ (   `' ,',' ,',' ,'     s:::::ssss::::::s     u:::::::::::::::uu       n::::n    n::::n");
     }
 
     /**
      * Draws the twelfth line of art
      */
     public static void drawLine12() {
-        System.out.println("  `.__,',' ,',' ,','  `.`.__,',' ,',' ,'       s::::::::::::::s       u:::::::::::::::u       n::::n    n::::n");
+        System.out.println(" `.__,',' ,',' ,','  `.`.__,',' ,',' ,'       s::::::::::::::s       u:::::::::::::::u       n::::n    n::::n");
     }
 
     /**
      * Draws the thirteenth line of art
      */
     public static void drawLine13() {
-        System.out.println("      .' ,',' ,' (   ,. `.  .' ,',' ,'          s:::::::::::ss         uu::::::::uu:::u       n::::n    n::::n");
+        System.out.println("     .' ,',' ,' (   ,. `.  .' ,',' ,'          s:::::::::::ss         uu::::::::uu:::u       n::::n    n::::n");
     }
 
     /**
      * Draws the fourteenth line of art
      */
     public static void drawLine14() {
-        System.out.println("       `',' ,','. `. `.`. `. `',' ,'             sssssssssss             uuuuuuuu  uuuu       nnnnnn    nnnnnn");
+        System.out.println("      `',' ,','. `. `.`. `. `',' ,'             sssssssssss             uuuuuuuu  uuuu       nnnnnn    nnnnnn");
     }
 
     /**
      * Draws the fifteenth line of art
      */
     public static void drawLine15() {
-        System.out.println("         `.',.`. `. `. `.`. `. `.'                      _                                     _");
+        System.out.println("        `.',.`. `. `. `.`. `. `.'                      _                                     _");
     }
 
     /**
      * Draws the sixteenth line of art
      */
     public static void drawLine16() {
-        System.out.println("           '. `.`. `. `. `.`. `,                       (_)                                   | |");
+        System.out.println("          '. `.`. `. `. `.`. `,                       (_)                                   | |");
     }
 
     /**
      * Draws the seventeenth line of art
      */
     public static void drawLine17() {
-        System.out.println("             `. `.`. `. `. `.`'              _ __ ___   _   ___  _ __  ___   ___  _   _  ___ | |_  ___  _ __ ___   ___");
+        System.out.println("            `. `.`. `. `. `.`'              _ __ ___   _   ___  _ __  ___   ___  _   _  ___ | |_  ___  _ __ ___   ___");
     }
 
     /**
      * Draws the eighteenth line of art
      */
     public static void drawLine18() {
-        System.out.println("               `. `.`. `. `,'               | '_ ` _ \\ | | / __|| '__|/ _ \\ / __|| | | |/ __|| __|/ _ \\| '_ ` _ \\ / __|");
+        System.out.println("              `. `.`. `. `,'               | '_ ` _ \\ | | / __|| '__|/ _ \\ / __|| | | |/ __|| __|/ _ \\| '_ ` _ \\ / __|");
     }
 
     /**
      * Draws the nineteenth line of art
      */
     public static void drawLine19() {
-        System.out.println("                 `. `'   )                  | | | | | || || (__ | |  | (_) |\\__ \\| |_| |\\__ \\| |_|  __/| | | | | |\\__ \\");
+        System.out.println("                `. `'   )                  | | | | | || || (__ | |  | (_) |\\__ \\| |_| |\\__ \\| |_|  __/| | | | | |\\__ \\");
     }
 
     /**
      * Draws the twentieth line of art
      */
     public static void drawLine20() {
-        System.out.println("                   `.__,'                   |_| |_| |_||_| \\___||_|   \\___/ |___/ \\__, ||___/ \\__|\\___||_| |_| |_||___/");
+        System.out.println("                  `.__,'                   |_| |_| |_||_| \\___||_|   \\___/ |___/ \\__, ||___/ \\__|\\___||_| |_| |_||___/");
     }
 
     /**
      * Draws the twenty-first line of art
      */
     public static void drawLine21() {
-        System.out.println("                                                                                   __/ |");
+        System.out.println("                                                                                  __/ |");
     }
 
     /**
      * Draws the twenty-second line of art
      */
     public static void drawLine22() {
-        System.out.println("                                                                                  |___/");
+        System.out.println("                                                                                 |___/");
     }
 }
