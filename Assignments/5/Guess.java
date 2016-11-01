2e3298db4f89fecf46554c1f2393b52332d52f44
Thomas <the1krutz@gmail.com>
2016-11-01 16:11:02 -0700

    starting assignment 5

---
+++ Assignments/5/Guess.java
@@ -0,0 +1,54 @@
+import java.util.*;
+
+public class Guess {
+    public static Random rand = new Random();
+    public static int maxGuess = 100;
+    public static Scanner console = new Scanner(System.in);
+
+    public static void main(String[] args) {
+
+    }
+
+    /**
+     * introduces the game to the computer player
+     * @required
+     */
+    public static void computerIntro() {
+        // give instructions on how to play, and how to quit
+    }
+
+    /**
+     * one iteration of the primary game loop
+     * @required
+     */
+    public static void gameLoop() {
+        // whole game end to end
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
